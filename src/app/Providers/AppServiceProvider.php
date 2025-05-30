ea9ad4513fe3ae600264da35615e615cdd8770f6
tlud2192 <tlud2192@gmail.com>
2025-05-29 22:05:55 -0600

    - changed for prod - updated light mode logo

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -22,8 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::unguard();
-        // if (env('APP_ENV') === 'production') {
-        //     URL::forceScheme('https');
-        // }
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
